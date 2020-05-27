35e46f97dd2582f82a937df709ebcfcc12e8477c
kwonelis <60756997+kwonelis@users.noreply.github.com>
2020-05-27 00:46:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import matplotlib.pyplot as plt 
 import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
