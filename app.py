650a34c58bd88e7eee896677715b9491d2d83070
kwonelis <60756997+kwonelis@users.noreply.github.com>
2020-05-27 16:06:54 -0400

    Update app.py
    remove matplotlib

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 import numpy as np
 import plotly.express as px
